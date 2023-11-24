6c5fdeb6a2d535e1b3b746682236a0a8d9febb34
Moisés Neto <moisesnetored@gmail.com>
2023-11-23 23:42:41 -0400

    feat: react server components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ProductProps {
   params: {
     data: string[]
@@ -7,11 +9,19 @@ interface ProductProps {
 export default function Product({ params }: ProductProps) {
   const [productId, size, color] = params.data
 
+  console.log('Ola', params)
+
+  function addToCard() {
+    console.log('Adicionou no carrinho')
+  }
+
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>color: {color}</p>
+
+      <button onClick={addToCard}>Carrinho</button>
     </div>
   )
 }
