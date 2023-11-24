8cdb46527da2d3ebc88304d27a4e7c38fc5799bc
Moisés Neto <moisesnetored@gmail.com>
2023-11-24 00:10:13 -0400

    feat: loading e streaming ssr

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,8 @@
-export default function Catalog() {
+export default async function Catalog() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('https://api.github.com/users/moisesnetouou')
+  const user = await response.json()
+
   return <h1>Catalog</h1>
 }
